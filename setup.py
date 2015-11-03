a4a1f05a73035ad821052139816cfbf3945b65f4
darryllane <darryllane101@gmail.com>
2015-11-03 15:35:15 +0000

    Update

--- setup.py
+++ setup.py
@@ -2,20 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='Bluto',
-    version='1.1.5',
+    version='1.1.6',
     author='Darryl lane',
     author_email='DarrylLane101@gmail.com',
     url='https://github.com/RandomStorm/Bluto',
     packages=find_packages() + ['Bluto'],
     include_package_data=True,
     license='LICENSE.txt',
-    description='DNS recon, brutfocing, DNS transfers.',
+    description='DNS recon, brutfocing, DNS transfers, Email Enumeration.',
     long_description=open('README.md').read(),
-    scripts=['Bluto/bluto.py'],
+    scripts=['Bluto/bluto'],
     install_requires=[
         "dnspython",
         "termcolor",
         "BeautifulSoup4",
+        "requests",
     ],
 )
 
