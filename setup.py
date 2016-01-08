937f5f0182b0f139ccbc5ae0ec167662b59cf793
Darryl <laned@mac.local>
2016-01-08 10:03:59 +0000

    Version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Bluto',
-    version='1.1.14',
+    version='1.1.15',
     author='Darryl lane',
     author_email='DarrylLane101@gmail.com',
     url='https://github.com/RandomStorm/Bluto',
